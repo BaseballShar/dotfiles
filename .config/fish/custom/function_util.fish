ff20709c29adc6a221019513d9142cc02fd165cc
Baseball Shar <workingcth@gmail.com>
2024-07-26 21:17:56 +0800

    Renamed yy to fm (file manager)

--- .config/fish/custom/function_util.fish
+++ .config/fish/custom/function_util.fish
@@ -32,3 +32,12 @@ function vm
     end
     cd "$dst"
 end
+
+function fm
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
