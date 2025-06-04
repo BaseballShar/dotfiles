4753eb1e7b8393d9a3f522791be349a1c296e307
Baseball Shar <workingcth@gmail.com>
2025-06-04 21:07:29 +0800

    Calender and detailed weather in Bar

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -4,5 +4,15 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
+if [ "$SENDER" = "mouse.entered" ]; then
+  sketchybar --set "$NAME" popup.drawing=on
+elif [ "$SENDER" = "mouse.exited" ]; then
+  sketchybar --set "$NAME" popup.drawing=off
+fi
+
 sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
 
+for rid in {1..6}; do
+  row=$(cal | awk "NR==$((rid+1))")
+  sketchybar --set "$NAME.$rid" label="$row" label.padding_right=10
+done
--- .config/sketchybar/plugins/weather.sh
+++ .config/sketchybar/plugins/weather.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-sketchybar --set "$NAME" label="$(curl wttr.in\?format=%c%t)"
+if [ "$SENDER" = "mouse.entered" ]; then
+  sketchybar --set "$NAME" popup.drawing=on
+elif [ "$SENDER" = "mouse.exited" ]; then
+  sketchybar --set "$NAME" popup.drawing=off
+fi
+
+# Centralised weather fetching
+weather_info="$(curl -s wttr.in\?format=%c+%t+%h+%p+%u)"
+
+# Extract relevant weather into cells
+weather_icon="$(echo $weather_info | cut -d' ' -f1)"
+temp="$(echo $weather_info | cut -d' ' -f2 | cut -d'+' -f2)"
+humidity="$(echo $weather_info | cut -d' ' -f3)"
+rain="$(echo $weather_info | cut -d' ' -f4)"
+uv="$(echo $weather_info | cut -d' ' -f5)"
+
+sketchybar --set "$NAME" label="$temp" icon="$weather_icon" \
+           --set "$NAME.humidity" label="$humidity" icon=󰖌 \
+           --set "$NAME.rain" label="$rain" icon= \
+           --set "$NAME.uv" label="$uv" icon=
