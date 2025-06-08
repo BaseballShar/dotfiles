43fd2ecbcd38cb8cd67a2e1eb2795de188a71ffe
Baseball Shar <workingcth@gmail.com>
2025-06-08 14:30:08 +0800

    Thematically coherent workspace indicator

--- .config/sketchybar/plugins/weather.sh
+++ .config/sketchybar/plugins/weather.sh
@@ -17,6 +17,6 @@ rain="$(echo $weather_info | cut -d' ' -f4)"
 uv="$(echo $weather_info | cut -d' ' -f5)"
 
 sketchybar --set "$NAME" label="$temp" icon="$weather_icon" \
-           --set "$NAME.humidity" label="$humidity" icon=󰖌 \
-           --set "$NAME.rain" label="$rain" icon= \
-           --set "$NAME.uv" label="$uv" icon=
+           --set "$NAME.humidity" label=" H: $humidity" label.padding_right=15 \
+           --set "$NAME.rain" label=" P: $rain" label.padding_right=15 \
+           --set "$NAME.uv" label="UV: $uv "
