c7158241534f3667644aafbbf9af2db78e9d3dc1
Baseball Chan <baseball.chan@spacious.hk>
2025-09-19 17:05:34 +0800

    more php snippet

--- .config/nvim/snippets/php.lua
+++ .config/nvim/snippets/php.lua
@@ -12,4 +12,51 @@ return {
     i(1),
     t("?>"),
   }),
+
+  s({ trig = "log", dscr = "Expand to console log" }, {
+    t("console.log("),
+    i(1),
+    t(")"),
+  }),
+
+  s({ trig = "if", dscr = "If statements" }, {
+    -- line 1
+    t("<?php if ("),
+    i(1),
+    t({ ") { ?>", "  " }),
+    -- line 2
+    i(2),
+    i(0),
+    -- line 3
+    t({ "", "<?php } ?>" }),
+  }),
+
+  s({ trig = "ife", dscr = "If else statements" }, {
+    -- line 1
+    t("<?php if ("),
+    i(1),
+    t({ ") { ?>", "  " }),
+    -- line 2
+    i(2),
+    i(0),
+    -- line 3
+    t({ "", "<?php } else {?>" }),
+    -- line 4
+    t({ "", "  " }),
+    i(3),
+    -- line 5
+    t({ "", "<?php } ?>" }),
+  }),
+
+  -- foreach
+  --  <?php foreach($column as $row) { ?>
+  --  <?php } ?>
+  s(
+    { trig = "for", dscr = "Foreach loops" },
+    fmt(
+      "<?php foreach([] as []) { ?>\n  []\n<?php } ?>",
+      { i(1, "items"), i(2, "item"), i(0, "loop content") },
+      { delimiters = "[]" }
+    )
+  ),
 }
---
+++ .config/zsh/rails.sh
@@ -0,0 +1,1 @@
+alias rs="bundle install; spring stop; rails db:migrate; yarn; git reset --hard; overmind s"
